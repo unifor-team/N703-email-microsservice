bed214a1d2a4eb122ff4ebb7699655134e6a5335
matheusponte <matheuspontes894@outlook.com>
2024-09-11 20:19:14 -0300

    Fix: consumers Email

--- src/main/java/com/email/ms_email/consumers/EmailConsumer.java
+++ src/main/java/com/email/ms_email/consumers/EmailConsumer.java
@@ -1,15 +1,27 @@
 package com.email.ms_email.consumers;
 
+import com.email.ms_email.EmailService.EmailService;
 import com.email.ms_email.dtos.EmailRecordDto;
+import com.email.ms_email.models.EmailModel;
+
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
 
-    @RabbitListener(queues = "${broker.queue.email.name}")
-    public void ListenEmailQueue(@Payload EmailRecordDto emailRecordDto){
-        System.out.println(emailRecordDto.emailTo());
+       final EmailService emailService;
+
+    public EmailConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @RabbitListener(queues = "${broker.queue.email.name}" )
+    public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto) {
+        var emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailRecordDto, emailModel);
+        emailService.sendEmail(emailModel);
     }
 }
--- src/main/java/com/email/ms_email/dtos/EmailRecordDto.java
+++ src/main/java/com/email/ms_email/dtos/EmailRecordDto.java
@@ -3,5 +3,6 @@ package com.email.ms_email.dtos;
 import java.util.UUID;
 
 public record EmailRecordDto(UUID userId, String emailTo, String subject, String text) {
+    
 
 }
